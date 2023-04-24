39a5f7e7f6035d9b39e08135039cb3b3923f547f
YuliaStAlawiyah <yuliasitialawiyah07@gmail.com>
2023-04-24 22:46:15 +0700

    initial & setup project

---
+++ movie_app/lib/constants.dart
@@ -0,0 +1,6 @@
+class Constants{
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrlw500 = 'https://image.tmdb.org/t/p/w500';
+  static const String imageUrloriginal = 'https://image.tmdb.org/t/p/original';
+  static const String apiKey = 'd8e957a69af1be921c9c4466f5661e87';
+}
